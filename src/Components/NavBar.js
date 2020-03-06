9218d7d12c95bb9f80e3bc1da752fa14599b18fc
Benjamin Huang <bhuang@rms.com>
2020-03-05 22:23:36 -0800

    update

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
-import { AppBar, Toolbar, IconButton, Button, Typography } from '@material-ui/core'
-import MenuIcon from '@material-ui/icons/Menu';
+import { AppBar, Toolbar, IconButton, Badge, Typography } from '@material-ui/core'
+import { AccountCircle, Mail as MailIcon, Menu as MenuIcon, Notifications as NotificationsIcon } from '@material-ui/icons';
+
 import { makeStyles } from '@material-ui/core/styles';
 
-const NavBar = () => {
+const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1,
+  },
+}));
 
+const NavBar = () => {
+  const classes = useStyles();
   return (
     <AppBar>
       <Toolbar position="static">
@@ -15,7 +22,26 @@ const NavBar = () => {
         <Typography color="inherit">
           React & Material-UI Application
           </Typography>
-        <Button color="inherit">Login</Button>
+          <div className={classes.grow} />
+        <IconButton aria-label="show 4 new mails" color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <MailIcon />
+          </Badge>
+        </IconButton>
+        <IconButton aria-label="show 17 new notifications" color="inherit">
+          <Badge badgeContent={17} color="secondary">
+            <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <IconButton edge="end"
+          aria-label="account of current user"
+          //aria-controls={menuId}
+          aria-haspopup="true"
+          //onClick={handleProfileMenuOpen}
+          color="inherit"
+        >
+          <AccountCircle />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
